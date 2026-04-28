4b1deb0b12b298245ad21855e9773306f31daaaf
Vasto <sonicdarkultra44@gmail.com>
2026-04-28 10:06:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,19 @@
 const cnv = document.getElementById("game-canvas");
 const ctx = cnv.getContext("2d");
 
+x = 0
+dx = 1
+
 funciton draw() {
+  ctx.clearRect(0, 0, cnv.width, cnv.height);
+  
+  ctx.fillStyle = "black";
+  ctx.fillRect(10, 10, x, cnv.height/2);
+
+  if (x < 0) dx = 1
+  if (x > cnv.width) dx = -1
+  x++;
+  
   requestAnimationFrame(draw);
 }
 draw()
