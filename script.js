fa669515ae00308ea7c8424fafb5c76765fba887
Vasto <sonicdarkultra44@gmail.com>
2026-04-28 10:04:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 /// SCRIPT.JS ANTI GRAVITY ///
+const cnv = document.getElementById("game-canvas");
+const ctx = cnv.getContext("2d");
+
+funciton draw() {
+  requestAnimationFrame(draw);
+}
+draw()
