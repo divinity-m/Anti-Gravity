7285b1079ee73d75a4a6a3c25db846dc3c5bdc85
Vasto <sonicdarkultra44@gmail.com>
2026-05-04 10:22:00 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,7 +1,21 @@
 /// FUNCTIONS.JS ANTI GRAVITY ///
 
+// Process Functions //
+function playerMovement() {
+    if (aPressed) {
+        if (!player.enteringPortal) player.x -= player.speed;
+
+        player.rotation -= player.spinSpeed;
+    }
+    if (dPressed) {
+        if (!player.enteringPortal) player.x += player.speed;
+    
+        player.rotation += player.spinSpeed;
+    }
+}
+
 function swapGravity() {
-    // swapGravity(): Determines the players falling direciton and changes the values of gravity and dGravity acoordingly
+    // Determines the falling direciton and changes the values of gravity and dGravity acoordingly
     if (!isMidAir) {
         fallingDirection = fallingDirection === "down" ? "up" : "down";
         gravity = fallingDirection === "down" ? 3 : -3;
@@ -9,9 +23,72 @@ function swapGravity() {
     }
 }
 
+function ImposeNaturalGravity(borderHeight) {
+    // ImposeNaturalGravity(): checks if the player is falling to apply the gravity mechanic
 
-// Draw functions
+    const midAirDown = player.y + player.r + gravity < cnv.height-borderHeight;
+    const midAirUp = player.y - player.r + gravity > borderHeight;
+    
+    isMidAir = fallingDirection === "down" ? midAirDown : midAirUp;
+    if (isMidAir && !player.enteringPortal) {
+        if (fallingDirection === "down") gravity = Math.min(gravity + dGravity, 10);
+        else gravity = Math.max(gravity + dGravity, -10);
 
+        player.y += gravity;
+    }
+
+    if (fallingDirection === "down" && !isMidAir) player.y = cnv.height-borderHeight - player.r;
+    if (fallingDirection === "up" && !isMidAir) player.y = borderHeight + player.r;
+}
+
+function ImposePortalGravity() {
+    // ImposePortalGravity(): imposes the portals 'pull' effect on the player when they get close enough
+
+    // calculate the distance of the player from the portal
+    const portalDx = portal.x - player.x;
+    const portalDy = portal.y - player.y;
+    const portalDist = Math.hypot(portalDx, portalDy);
+    
+    const portalRange = portal.r + 50;
+
+    // depiction of the portal's range
+    ctx.strokeStyle = "blue";
+    drawCircle(portal.x, portal.y, portalRange, 2);
+
+    if (portalDist < portalRange) {
+        player.enteringPortal = true;
+        player.spinSpeed =  Math.PI/32;
+
+        // the angle from the player to the portal
+        const angleToPortal = Math.atan2(portalDy, portalDx);
+
+        // gets the angular difference then normalizes it
+        let dAngle = angleToPortal - player.facingAngle;
+        dAngle = Math.atan2(Math.sin(dAngle), Math.cos(dAngle));
+
+        // get a turn speed proportional to the distance from the player to the portal
+        const turnSpeed = 0.1;
+
+        // add either the dAngle or the turnSpeed to the players angle
+        player.facingAngle += Math.sign(dAngle) * Math.min(Math.abs(dAngle), turnSpeed);
+        
+        const clampSpeed = Math.max(portalDist / portalRange, 0.1);
+
+        // move the player in the direciton of the angle
+        player.x += Math.cos(player.facingAngle) * player.speed * clampSpeed;
+        player.y += Math.sin(player.facingAngle) * player.speed * clampSpeed;
+    }
+    else {
+        player.enteringPortal = false;
+        player.spinSpeed =  Math.PI/16;
+    }
+}
+
+function proceedToNextLevel() {
+
+}
+
+// Draw Functions //
 function drawCircle(x, y, r, lw = 0) {
     // takes in the 'x' and 'y' parameters for location and the 'r' and 'fill' parameters for design
 
@@ -24,34 +101,24 @@ function drawCircle(x, y, r, lw = 0) {
     }
 }
 
-function drawPlayer() {
-    // drawPlayer(): draws the player and handles it's rotation
+function drawPlayer(x, y, r, rotation) {
+    // drawPlayer(): draws the player
 
     ctx.save();
     ctx.translate(player.x, player.y);
     ctx.rotate(player.rotation);
-
     ctx.drawImage(document.getElementById("grey-ball"), -player.r * 1.5, -player.r * 1.5, player.r * 3, player.r * 3);
-
     ctx.restore();
 }
 
-function imposeGravity(borderHeight) {
-    // imposeGravity(): checks if the player is falling to apply the gravity mechanic
+function drawPortal() {
+    // drawPlayer(): draws the portal and handles it's rotation
 
-    if (player.enteringPortal) return;
+    ctx.save();
+    ctx.translate(portal.x, portal.y)
+    ctx.rotate(portal.rotation);
+    ctx.drawImage(document.getElementById("portal-img"), -portal.r * 1.5, -portal.r * 1.5, portal.r * 3, portal.r * 3);
+    ctx.restore();
 
-    const midAirDown = player.y + player.r + gravity < cnv.height-borderHeight;
-    const midAirUp = player.y - player.r + gravity > borderHeight;
-    
-    isMidAir = fallingDirection === "down" ? midAirDown : midAirUp;
-    if (isMidAir) {
-        if (fallingDirection === "down") gravity = Math.min(gravity + dGravity, 10);
-        else gravity = Math.max(gravity + dGravity, -10);
-
-        player.y += gravity;
-    }
-
-    if (fallingDirection === "down" && !isMidAir) player.y = cnv.height-borderHeight - player.r;
-    if (fallingDirection === "up" && !isMidAir) player.y = borderHeight + player.r;
+    portal.rotation += portal.spinSpeed;
 }
